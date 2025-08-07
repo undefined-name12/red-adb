d65a8acad560f70b85d97f0a90399258cfe46de6
undefined_name <70478422+undefined-name12@users.noreply.github.com>
2025-08-07 23:36:17 +0200

    Update fetch.py

--- extra/fetch.py
+++ extra/fetch.py
@@ -1,7 +1,7 @@
 import requests
 import os
 
-shodankey = "" # add your key here
+shodankey = "" # agrega tu clave aquí
 
 ip_req = requests.get(f"https://api.shodan.io/shodan/host/search?key={shodankey}&query=android+debug+bridge").text
 print(ip_req)
